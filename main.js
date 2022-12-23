a9001a4ad82dd048a4c8a108290bef4f03ba2cbe
Harold Sanchez <sanchezharold13@gmail.com>
2022-12-23 09:43:31 -0500

    Fixed the bugs

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 let textArea = document.getElementById("textarea");
+let textoEncriptado = document.getElementById("texto-encriptado");
 const btnEncriptar = document.getElementById("encriptar");
 const btnDesEncriptar = document.getElementById("desencriptar");
 const btnCopiar = document.getElementById("copiar");
 const textos = document.getElementById("textos");
-let textoEncriptado = document.getElementById("texto-encriptado");
 
 btnEncriptar.addEventListener("click", () => {
   if (textArea.value !== '') {
     textoEncriptado.innerText = encriptar(textArea.value);
     textos.style.display = "none";
     btnCopiar.style.display = 'block';
-    textos.style.transition = ".3s linear";
+    textoEncriptado.style.display = 'block';
   } else {
-    textos.style.display = 'block';
+    textos.style.display = 'flex';
     textoEncriptado.innerText = '';
+    textoEncriptado.style.display = 'none';
   }
 });
 
@@ -22,35 +23,17 @@ btnDesEncriptar.addEventListener('click', ()=>{
     textoEncriptado.innerText = desEncriptar(textArea.value);
     textos.style.display = "none";
     btnCopiar.style.display = 'block';
-    textos.style.transition = ".3s linear";
+    textoEncriptado.style.display = 'block';
   } else {
-    textos.style.display = 'block';
+    textos.style.display = 'flex';
     textoEncriptado.innerText = '';
+    textoEncriptado.style.display = 'none';
   }
 })
 
 btnCopiar.addEventListener('click', ()=>{
   navigator.clipboard.writeText(textoEncriptado.textContent)
 })
-
-function encriptar(palabra){
-  let arreglo = [... palabra];
-
-  for(let i = 0; i < arreglo.length; i++){
-    if(arreglo[i] === 'a'){
-      arreglo[i] = 'ai';
-    } else if(arreglo[i] === 'e'){
-      arreglo[i] = 'enter';
-    } else if(arreglo[i] === 'i'){
-      arreglo[i] = 'imes';
-    } else if(arreglo[i] === 'o'){
-      arreglo[i] = 'ober';
-    } else if(arreglo[i] === 'u'){
-      arreglo[i] = 'ufat';
-    }
-  }
-  return arreglo.join('');
-}
 
 const diccionarioEncriptado = {
   'a': 'ai',
@@ -60,42 +43,7 @@ const diccionarioEncriptado = {
   'u': 'ufat'
 }
 
-const diccionarioDesEncriptado = {
-  'ai': 'a',
-  'enter': 'e',
-  'imes': 'i',
-  'ober': 'o',
-  'ufat': 'u'
-}
-
-function desEncriptar(texto){
-  let textoDesEncriptado = '';
-  let palabraActual = '';
-
-  for(let i = 0; i < texto.length; i++){
-    const letra = texto[i];
-    if(letra in diccionarioDesEncriptado){
-      palabraActual += letra
-    } else {
-      if(palabraActual in diccionarioDesEncriptado){
-        textoDesEncriptado += diccionarioDesEncriptado[palabraActual];
-      } else{
-        textoDesEncriptado += palabraActual;
-      }
-      palabraActual = '';
-      textoDesEncriptado += letra;
-    }
-  }
-
-  if(palabraActual in diccionarioDesEncriptado){
-    textoDesEncriptado += diccionarioDesEncriptado[palabraActual]
-  } else {
-    textoDesEncriptado += palabraActual;
-  }
-  return textoDesEncriptado;
-} 
-
-function encrypt(texto){
+function encriptar(texto){
   let encriptedText = '';
   for(let i = 0; i<texto.length; i++){
     const letter = texto[i];
@@ -108,8 +56,11 @@ function encrypt(texto){
   return encriptedText;
 }
 
-console.log(desEncriptar('Hoberlai'))
+function desEncriptar(texto){
+  let palabra = '';
+  if(texto !== ''){
+    palabra += texto.replaceAll('ai','a').replaceAll('enter','e').replaceAll('imes','i').replaceAll('ober','o').replaceAll('ufat','u')
+  }
+  return palabra
+} 
 
-/*
- *asdwasdwas
-*/
